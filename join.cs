3406a9ff7f013b83d62eb655e543ec331b8b8e0a
Joachim Brolin <jbrolin90@gmail.com>
2024-06-17 15:32:53 +0200

    Experimenting...

--- join.cs
+++ join.cs
@@ -1,11 +1,13 @@
 using System.Data;
 
 namespace JoinTables;
+// q: What is this class?
 
 
-public partial class Join :DataTable
+
+public partial class Join : DataTable
 {
-    
+
         public Join(DataSet joinSet) => Init(joinSet);
         public void Fill() => _Fill();
         public void SelectAll() => _SelectAll();
--- joinOuter.cs
+++ joinOuter.cs
@@ -2,24 +2,34 @@ using System.Data;
 namespace JoinTables;
 public partial class Join :DataTable
 {
-    public void LeftOuter(Func<DataSet, DataRow, DataRow?> test)
+    // Method that uses Linq to join table
+    public void JoinOuter(DataTable table1, DataTable table2 )
     {
-        foreach(DataRow leftRow in joinSet.Tables[0].Rows)
+        var query = from t1 in table1.AsEnumerable()
+                    join t2 in table2.AsEnumerable()
+                    on t1.Field<int>("ID") equals t2.Field<int>("ID") into temp
+        foreach (var item in query)
         {
-            DataRow resultRow = NewRow();
-            foreach (DataColumn c in joinSet.Tables[0].Columns)
-            {
-                resultRow[joinSet.Tables[0].FQColumnName(c)] = leftRow[c.ColumnName];
-            }
-            Rows.Add(resultRow);
-            DataRow? rightRow = test(joinSet, leftRow);
-            if(rightRow != null)
-            {
-                foreach (DataColumn c in rightRow.Table.Columns)
-                {
-                    resultRow[rightRow.Table.FQColumnName(c)] = rightRow[c.ColumnName];
-                }
-            }
+            Console.WriteLine($"ID: {item.ID}, Name: {item.Name}, Age: {item.Age}, Address: {item.Address}");
         }
-    }
+   }
+    public void XJoinOuter(DataTable table1, DataTable table2 )
+    {
+        var query = from t1 in table1.AsEnumerable()
+                    join t2 in table2.AsEnumerable()
+                    on t1.Field<int>("ID") equals t2.Field<int>("ID") into temp
+                    from t2 in temp.DefaultIfEmpty()
+                    select new
+                    {
+                        ID = t1.Field<int>("ID"),
+                        Name = t1.Field<string>("Name"),
+                        Age = t1.Field<int>("Age"),
+                        Address = t2 == null ? "No Address" : t2.Field<string>("Address")
+                    };
+        foreach (var item in query)
+        {
+            Console.WriteLine($"ID: {item.ID}, Name: {item.Name}, Age: {item.Age}, Address: {item.Address}");
+        }
+   }
+
 }
